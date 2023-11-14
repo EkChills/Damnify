7b11b8e75e75c8f0dcec64ec2e6f3b696797e295
Ekene <megafat142@gmail.com>
2023-11-14 08:42:34 +0100

    changes in createchat api

--- src/app/api/createchat/route.ts
+++ src/app/api/createchat/route.ts
@@ -1,3 +1,4 @@
+
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "../../../../prisma/prismaclient";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
